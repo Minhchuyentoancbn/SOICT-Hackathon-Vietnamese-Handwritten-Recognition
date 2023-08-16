7cab9052e8a17445fa686580fb53c60a3e3ca1d3
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-16 23:45:27 +0700

    Add initialize parameters

--- train.py
+++ train.py
@@ -4,12 +4,14 @@ from pytorch_lightning.callbacks import EarlyStopping
 
 import sys
 import warnings
+import pandas as pd
 from argparse import ArgumentParser
 from models.crnn import CRNN
 from models.ctc_baseline import CTCBaseline
-from utils import get_device
-from data_loader import get_data, IMG_WIDTH, IMG_HEIGHT
+from data_loader import get_data, IMG_WIDTH, IMG_HEIGHT, HandWritttenDataset
 from config import NUM_CLASSES
+from predict import predict
+from utils import initilize_parameters
 
 
 def parse_arguments(argv):
@@ -18,12 +20,20 @@ def parse_arguments(argv):
     parser.add_argument('--val_check_interval', type=float, default=0.25)
     parser.add_argument('--model_name', type=str)
 
+    # Optimization hyperparameters
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--optimizer', type=str, default='adam')
     parser.add_argument('--weight_decay', type=float, default=1e-4)
     parser.add_argument('--dropout', type=float, default=0.5)
 
+    # Learning rate scheduler
+    parser.add_argument('--warmup_steps', type=int, default=0)
+
+
+    # Decode hyperparameters
     parser.add_argument('--decode_method', type=str, default='beam_search')
     parser.add_argument('--beam_size', type=int, default=3)
 
@@ -31,11 +41,13 @@ def parse_arguments(argv):
 
 
 def crnn(args):
-    device = get_device()
     train_loader, val_loader, test_loader = get_data(args.batch_size, args.seed)
+
+    pl.seed_everything(args.seed)
 
     # model
     crnn = CRNN(3, IMG_HEIGHT, IMG_WIDTH, NUM_CLASSES)
+    crnn = initilize_parameters(crnn)
     plcrnn = CTCBaseline(crnn, args)
 
     early_stop_callback = EarlyStopping(
@@ -45,7 +57,6 @@ def crnn(args):
     # train model
     trainer = pl.Trainer(
         default_root_dir=f'checkpoints/{args.model_name}/',
-        gradient_clip_val=5, #callbacks=[early_stop_callback],
         max_epochs=args.epochs,
         val_check_interval=args.val_check_interval,
     )
@@ -55,6 +66,15 @@ def crnn(args):
 
     # Save models
     torch.save(plcrnn.model.state_dict(), f'saved_models/{args.model_name}.pt')
+
+    print('Predicting...')
+    print('-' * 50)
+
+    label2char = HandWritttenDataset.LABEL2CHAR
+    preds, img_names = predict(plcrnn, test_loader, label2char, args.decode_method, args.beam_size)
+
+    df = pd.DataFrame({'file_name': img_names, 'pred': preds})
+    df.to_csv(f'predictions/{args.model_name}.txt', index=False, header=False, sep='\t')
 
 
 if __name__ == '__main__':
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import pickle
 import torch
+import torch.nn as nn
 
 def load_char_dict():
     # Load char_dict from file
@@ -11,3 +12,24 @@ def get_device():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'device: {device}')
     return device
+
+def initilize_parameters(model):
+    # Initialize weights
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.weight, 1)
+        elif isinstance(m, nn.Linear):
+            nn.init.xavier_normal_(m.weight)
+            nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LSTM):
+            for name, param in m.named_parameters():
+                if 'weight_ih' in name:
+                    nn.init.xavier_normal_(param.data)
+                elif 'weight_hh' in name:
+                    nn.init.orthogonal_(param.data)
+                elif 'bias' in name:
+                    nn.init.constant_(param.data, 0.0)
+
+    return model
