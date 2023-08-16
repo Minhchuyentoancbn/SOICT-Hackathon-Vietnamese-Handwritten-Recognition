b02dad948f21a5c0eae5664f2d2fdcd50d789018
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-16 18:25:36 +0700

    Add Kaggle dir

--- data_loader/config.py
+++ data_loader/config.py
@@ -1,7 +1,11 @@
 # Config paths
-TRAIN_DIR = 'data/new_train/'
-PUBLIC_TEST_DIR = 'data/new_public_test/'
+# TRAIN_DIR = 'data/new_train/'
+# PUBLIC_TEST_DIR = 'data/new_public_test/'
 LABEL_FILE = 'data/train_gt.txt'
+
+# For Kaggle
+TRAIN_DIR = '/kaggle/input/ocr-bkai-hackathon/training_data/new_train'
+PUBLIC_TEST_DIR = '/kaggle/input/ocr-bkai-hackathon/public_test_data/new_public_test'
 
 # Image size
 IMG_HEIGHT = 32
