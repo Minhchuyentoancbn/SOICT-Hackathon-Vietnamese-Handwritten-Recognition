7be38a1657fd7c3c905c91d9ce104693b54930e3
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-17 10:34:12 +0700

    Update rules

--- models/utils.py
+++ models/utils.py
@@ -1,8 +1,8 @@
 def rule(args):
     train_steps = 93100 // args.batch_size * args.epochs
-    WARM_UP_STEP = args.warmup_steps
+    WARM_UP_STEP = train_steps / 2
     def lr_update_rule(step):
-        if step < 7000:
+        if step < WARM_UP_STEP:
             return  1.0
         else:
             return (train_steps - step)/(train_steps - WARM_UP_STEP)
