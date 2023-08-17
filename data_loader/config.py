b7cba16a2679ca8629e767a242621e5319533281
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-17 21:10:03 +0700

    Remove width and height

--- data_loader/config.py
+++ data_loader/config.py
@@ -6,7 +6,3 @@ LABEL_FILE = 'data/train_gt.txt'
 # For Kaggle
 TRAIN_DIR = '/kaggle/input/ocr-bkai-hackathon/training_data/new_train'
 PUBLIC_TEST_DIR = '/kaggle/input/ocr-bkai-hackathon/public_test_data/new_public_test'
-
-# Image size
-IMG_HEIGHT = 64
-IMG_WIDTH = 256
