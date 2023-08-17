a3bccc4f4cce6e955e485fe6b52230742b812271
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-17 11:33:13 +0700

    Try different resolution

--- data_loader/config.py
+++ data_loader/config.py
@@ -8,5 +8,5 @@ TRAIN_DIR = '/kaggle/input/ocr-bkai-hackathon/training_data/new_train'
 PUBLIC_TEST_DIR = '/kaggle/input/ocr-bkai-hackathon/public_test_data/new_public_test'
 
 # Image size
-IMG_HEIGHT = 32
-IMG_WIDTH = 128
+IMG_HEIGHT = 64#32
+IMG_WIDTH = 256#128
