1280ce11bff092e5781810b6c6613b32d71f9ae2
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-18 15:04:32 +0700

    Split files

--- train.py
+++ train.py
@@ -1,18 +1,7 @@
-import torch
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import EarlyStopping
-
 import sys
 import warnings
-import pandas as pd
 from argparse import ArgumentParser
-from models.crnn import CRNN
-from models.cnnctc import CNNCTC
-from models.ctc_baseline import CTCBaseline
-from data_loader import get_data, HandWritttenDataset
-from config import NUM_CLASSES
-from predict import predict
-from utils import initilize_parameters, make_submission
+from train_utils import ctc, safl
 
 
 def parse_arguments(argv):
@@ -46,56 +35,6 @@ def parse_arguments(argv):
     return parser.parse_args(argv)
 
 
-def ctc(args):
-    train_loader, val_loader, test_loader = get_data(args.batch_size, args.seed, args)
-
-    pl.seed_everything(args.seed)
-
-    # model
-    if args.model_name == 'crnn':
-        model = CRNN(3, args.height, args.width, NUM_CLASSES, dropout=args.dropout)
-        model = initilize_parameters(model)
-    elif args.model_name == 'cnnctc':
-        model = CNNCTC(NUM_CLASSES)
-
-    pl_model = CTCBaseline(model, args)
-
-    early_stop_callback = EarlyStopping(
-        monitor='val_loss',
-        mode='min', patience=10, verbose=True
-    )
-
-    if args.train:
-        # train model
-        trainer = pl.Trainer(
-            default_root_dir=f'checkpoints/{args.model_name}/',
-            max_epochs=args.epochs,
-            val_check_interval=args.val_check_interval,
-        )
-        trainer.fit(
-            model=pl_model, train_dataloaders=train_loader, val_dataloaders=val_loader
-        )
-    else:
-        # train model
-        trainer = pl.Trainer(
-            default_root_dir=f'checkpoints/{args.model_name}/',
-            max_epochs=args.epochs,
-        )
-        trainer.fit(
-            model=pl_model, train_dataloaders=train_loader
-        )
-
-    # Save models
-    torch.save(pl_model.model.state_dict(), f'saved_models/{args.model_name}.pt')
-
-    print('Predicting...')
-    print('-' * 50)
-
-    # Make submission
-    label2char = HandWritttenDataset.LABEL2CHAR
-    preds, img_names = predict(pl_model, test_loader, label2char, args.decode_method, args.beam_size)
-    make_submission(preds, img_names, args)
-
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
@@ -103,3 +42,5 @@ if __name__ == '__main__':
 
     if args.model_name in ['crnn', 'cnnctc']:
         ctc(args)
+    elif args.model_name == 'safl':
+        safl(args)
