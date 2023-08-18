b4316c8771b53d416cfdef745852f1c287a2981d
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-18 10:50:33 +0700

    Switch account

--- data_loader/config.py
+++ data_loader/config.py
@@ -1,8 +1,11 @@
 # Config paths
+LABEL_FILE = 'data/train_gt.txt'
 # TRAIN_DIR = 'data/new_train/'
 # PUBLIC_TEST_DIR = 'data/new_public_test/'
-LABEL_FILE = 'data/train_gt.txt'
 
 # For Kaggle
-TRAIN_DIR = '/kaggle/input/ocr-bkai-hackathon/training_data/new_train'
-PUBLIC_TEST_DIR = '/kaggle/input/ocr-bkai-hackathon/public_test_data/new_public_test'
+# TRAIN_DIR = '/kaggle/input/ocr-bkai-hackathon/training_data/new_train'
+# PUBLIC_TEST_DIR = '/kaggle/input/ocr-bkai-hackathon/public_test_data/new_public_test'
+
+TRAIN_DIR = '/kaggle/input/handwritten-ocr-hackathon/training_data/new_train'
+PUBLIC_TEST_DIR = '/kaggle/input/handwritten-ocr-hackathon/public_test_data/new_public_test'
