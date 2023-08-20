770f4b2419d11c95ee4b8dc6306d7454925c24ba
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-21 00:16:27 +0700

    Refactor code

--- models/utils.py
+++ models/utils.py
@@ -1,4 +1,5 @@
 from torch.nn import init
+from argparse import ArgumentParser
 
 def rule(args):
     if args.train:
@@ -15,6 +16,45 @@ def rule(args):
         else:
             return 0.1 #(train_steps - step)/(train_steps - WARM_UP_STEP)
     return lr_update_rule
+
+
+def parse_arguments(argv):
+    parser = ArgumentParser()
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--val_check_interval', type=float, default=0.25)
+    parser.add_argument('--model_name', type=str)
+    parser.add_argument('--train', type=int, default=1)
+    parser.add_argument('--num_samples', type=int, default=0)
+
+    # Optimization hyperparameters
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--optimizer', type=str, default='adam')
+    parser.add_argument('--weight_decay', type=float, default=1e-4)
+    parser.add_argument('--dropout', type=float, default=0.0)
+
+    # Learning rate scheduler
+    parser.add_argument('--warmup_steps', type=int, default=0)
+
+    # Data processing
+    parser.add_argument('--resize', type=int, default=1)
+    parser.add_argument('--height', type=int, default=64)
+    parser.add_argument('--width', type=int, default=256)
+
+    # CTC decode hyperparameters
+    parser.add_argument('--decode_method', type=str, default='beam_search')
+    parser.add_argument('--beam_size', type=int, default=3)
+    parser.add_argument('--feature_extractor', type=str, default='vgg')
+
+    # Encoder decoder hyperparameters
+    parser.add_argument('--stn_on', type=int, default=0)
+    parser.add_argument('--max_len', type=int, default=20)
+    parser.add_argument('--label_smoothing', type=float, default=0.0)
+
+    return parser.parse_args(argv)
+
 
 
 def initialize_weights(model):
--- train.py
+++ train.py
@@ -1,45 +1,8 @@
 import torch
 import sys
 import warnings
-from argparse import ArgumentParser
-from train_utils import ctc, safl
-
-
-def parse_arguments(argv):
-    parser = ArgumentParser()
-    parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--val_check_interval', type=float, default=0.25)
-    parser.add_argument('--model_name', type=str)
-    parser.add_argument('--train', type=int, default=1)
-    parser.add_argument('--num_samples', type=int, default=0)
-
-    # Optimization hyperparameters
-    parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--momentum', type=float, default=0.9)
-    parser.add_argument('--optimizer', type=str, default='adam')
-    parser.add_argument('--weight_decay', type=float, default=1e-4)
-    parser.add_argument('--dropout', type=float, default=0.0)
-
-    # Learning rate scheduler
-    parser.add_argument('--warmup_steps', type=int, default=0)
-
-    # Data processing
-    parser.add_argument('--resize', type=int, default=1)
-    parser.add_argument('--height', type=int, default=64)
-    parser.add_argument('--width', type=int, default=256)
-
-    # CTC decode hyperparameters
-    parser.add_argument('--decode_method', type=str, default='beam_search')
-    parser.add_argument('--beam_size', type=int, default=3)
-    parser.add_argument('--feature_extractor', type=str, default='vgg')
-
-    # SAFL hyperparameters
-    parser.add_argument('--stn_on', type=int, default=0)
-
-    return parser.parse_args(argv)
-
+from train_utils import ctc, encoder_decoder
+from models.utils import parse_arguments
 
 
 if __name__ == '__main__':
@@ -51,5 +14,5 @@ if __name__ == '__main__':
 
     if args.model_name in ['crnn', 'cnnctc']:
         ctc(args)
-    elif args.model_name == 'safl':
-        safl(args)
+    elif args.model_name in ['aed']:
+        encoder_decoder(args)
