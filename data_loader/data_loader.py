6cd8189bd6ccf5b54f9e75327f655ccb21e1d4f7
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-21 10:34:46 +0700

    Remove grayscale

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -42,7 +42,7 @@ def get_data(
             # Random Rotation
             transforms.RandomRotation(15),
             # Radom Grayscale
-            transforms.RandomGrayscale(p=0.2),
+            # transforms.RandomGrayscale(p=0.2),
             transforms.Resize((args.height, args.width)),
             transforms.ToTensor(),
             transforms.Normalize(
@@ -65,7 +65,7 @@ def get_data(
             # Random Rotation
             transforms.RandomRotation(15),
             # Radom Grayscale
-            transforms.RandomGrayscale(p=0.2),
+            # transforms.RandomGrayscale(p=0.2),
             FixedHeightResize(args.height), 
             FixedWidthPad(args.width),
             transforms.ToTensor(),
