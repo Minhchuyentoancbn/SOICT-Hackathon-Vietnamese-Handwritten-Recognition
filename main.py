2912d1846c4e36479d9f72e2e0aaa06fbd096ce6
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-23 16:52:14 +0700

    Move parse arguments to main

--- main.py
+++ main.py
@@ -3,7 +3,51 @@ import pytorch_lightning as pl
 import sys
 import warnings
 from train import train
-from utils import parse_arguments
+from argparse import ArgumentParser
+
+
+def parse_arguments(argv):
+    parser = ArgumentParser()
+    # Training hyperparameters
+    parser.add_argument('--seed', type=int, default=42, help='Random seed, default: 42')
+    parser.add_argument('--val_check_interval', type=float, default=0.5, help='Validation check interval per epoch, default: 0.5')
+    parser.add_argument('--model_name', type=str, help='Name of the model to train')
+
+    # Optimization hyperparameters
+    parser.add_argument('--epochs', type=int, default=5, help='Number of epochs, default: 5')
+    parser.add_argument('--batch_size', type=int, default=64, help='Batch size, default: 64')
+    parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate, default: 1e-3')
+    parser.add_argument('--momentum', type=float, default=0.9, help='Momentum, default: 0.9')
+    parser.add_argument('--optim', type=str, default='adam', help='Optimizer, default: adam, options: adam, sgd, adamw, adadelta')
+    parser.add_argument('--weight_decay', type=float, default=0.0, help='Weight decay, default: 0.0')
+    parser.add_argument('--dropout', type=float, default=0.0, help='Dropout after image feature extractor, default: 0.0')
+    parser.add_argument('--label_smoothing', type=float, default=0.0, help='Label smoothing for CrossEntropyLoss, default: 0.0')
+
+    # Learning rate scheduler
+    parser.add_argument('--scheduler', type=int, default=0, help='Whether to use learning rate scheduler or not, default: 0 (not use scheduler)')
+    parser.add_argument('--decay_epochs', type=int, default=0, help='Decay learning rate after this number of epochs, default: 0')
+
+    # Data processing
+    parser.add_argument('--train', type=int, default=1, help='Whether to use all training data or not, default: 1 (not use all training data)')
+    parser.add_argument('--num_samples', type=int, default=0, help='Number of samples to train on, default: 0 (all samples)')
+    parser.add_argument('--keep_ratio_with_pad', type=int, default=0, help='Whether to keep ratio for image resize, default: 0 (keep ratio)')
+    parser.add_argument('--height', type=int, default=32, help='Height of the input image, default: 32')
+    parser.add_argument('--width', type=int, default=128, help='Width of the input image, default: 128')
+    parser.add_argument('--grayscale', type=int, default=0, help='Convert image to grayscale which then has 1 channel, default: 0')
+
+    # Model hyperparameters
+    # VitSTR
+    choices = ["vitstr_tiny_patch16_224", "vitstr_small_patch16_224", "vitstr_base_patch16_224", "vitstr_tiny_distilled_patch16_224", "vitstr_small_distilled_patch16_224"]
+    parser.add_argument('--transformer', type=int, default=0, help='Whether to use transformer or not, default: 0 (not use transformer)')
+    parser.add_argument('--transformer_model', type=str, default=choices[0], help='VitSTR model, default: vitstr_tiny_patch16_224, options: vitstr_tiny_patch16_224, vitstr_small_patch16_224, vitstr_base_patch16_224, vitstr_tiny_distilled_patch16_224, vitstr_small_distilled_patch16_224')
+
+    # CTC and Attention
+    parser.add_argument('--feature_extractor', type=str, default='resnet', help='Feature extractor, default: resnet, options: resnet, vgg')
+    parser.add_argument('--stn_on', type=int, default=0, help='Whether to use STN or not, default: 0 (not use STN)')
+    parser.add_argument('--prediction', type=str, default='ctc', help='Prediction method, default: ctc, options: ctc, attention')
+    parser.add_argument('--max_len', type=int, default=25, help='Max length of the predicted text, default: 25')
+
+    return parser.parse_args(argv)
 
 
 if __name__ == '__main__':
