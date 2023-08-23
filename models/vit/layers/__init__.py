c51d56702621526300c9ecf821a38f0b16d39342
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-23 10:58:49 +0700

    Add ViT

---
+++ models/vit/layers/__init__.py
@@ -0,0 +1,7 @@
+from .patch_embed import PatchEmbed, resample_patch_embed
+from .mlp import Mlp
+from .drop import DropPath
+from .weight_init import trunc_normal_,  lecun_normal_
+from .pos_embed import resample_abs_pos_embed
+from .patch_dropout import PatchDropout
+from .config import use_fused_attn
