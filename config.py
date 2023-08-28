bb3c7b5b272e47ffba1e855668738a6e568391b5
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-28 14:16:29 +0700

    Update path

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 # Config paths
 LABEL_FILE = 'data/train_gt.txt'
-TRAIN_DIR = 'data/new_train/'
-PUBLIC_TEST_DIR = 'data/new_public_test/'
-
-# SynthText
 SYNTH_LABEL_FILE = 'data/generated_train.txt'
-SYNTH_TRAIN_DIR = 'data/two_characters/'
+# TRAIN_DIR = 'data/new_train/'
+# PUBLIC_TEST_DIR = 'data/new_public_test/'
+# SYNTH_TRAIN_DIR = 'data/two_characters/'  # SynthText
 
 # For Kaggle
 # TRAIN_DIR = '/kaggle/input/ocr-bkai-hackathon/training_data/new_train'
@@ -16,3 +14,7 @@ SYNTH_TRAIN_DIR = 'data/two_characters/'
 
 # TRAIN_DIR = '/kaggle/input/handwritten-ocr/training_data/new_train'
 # PUBLIC_TEST_DIR = '/kaggle/input/handwritten-ocr/public_test_data/new_public_test'
+
+PUBLIC_TEST_DIR = '/kaggle/input/handwritten-ocr/public_test_data/new_public_test'
+TRAIN_DIR = '/kaggle/input/handwritten-ocr/training_data/new_train'
+SYNTH_TRAIN_DIR = '/kaggle/input/handwritten-ocr/two_characters/two_characters'
