a5b2f5a671dfa0b10e30a69f8611f8db4a83b160
Le Duc Minh <minhmeo7a1@gmail.com>
2023-08-29 11:54:52 +0700

    Add mark counter

---
+++ models/counter.py
@@ -0,0 +1,130 @@
+import torch.nn as nn
+
+class MarkCounter(nn.Module):
+    def __init__(self, input_channel):
+        super(MarkCounter, self).__init__()
+        self.input_channel = input_channel
+        self.conv1 = nn.Conv2d(input_channel, 512, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(512)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        self.conv3 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.linear = nn.Linear(128, 5)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.avgpool(x)
+        x.squeeze_(3).squeeze_(2)
+        x = self.linear(x)
+        return x
+
+
+
+# class Radical_counter_N(nn.Module):
+#     def __init__(self):
+#         super(Radical_counter_N, self).__init__()
+#         self.conv1 = nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding=1)
+#         self.bn1 = nn.BatchNorm2d(512)
+#         self.relu1 = nn.ReLU(inplace=True)
+
+#         self.conv2 = nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1)
+#         self.bn2 = nn.BatchNorm2d(256)
+#         self.relu2 = nn.ReLU(inplace=True)
+
+#         self.conv3 = nn.Conv2d(256, 64, kernel_size=3, stride=1, padding=1)
+#         self.bn3 = nn.BatchNorm2d(64)
+#         self.relu3 = nn.ReLU(inplace=True)
+
+#         self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+
+#         if config['rn_loss']=='L1':
+#             self.linear = nn.Linear(64, 1)
+#         else:
+#             self.linear = nn.Linear(64, 11)
+#             self.softmax = nn.Softmax(dim=1)
+
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.bn1(x)
+#         x = self.relu1(x)
+
+#         x = self.conv2(x)
+#         x = self.bn2(x)
+#         x = self.relu2(x)
+
+#         x = self.conv3(x)
+#         x = self.bn3(x)
+#         x = self.relu3(x)
+
+#         x = self.avgpool(x)
+#         x.squeeze_(3).squeeze_(2)
+#         if config['rn_loss'] == 'L1':
+#             x = self.linear(x)
+#             x.squeeze_(1)
+#         else:
+#             x = self.linear(x)
+#             x = self.softmax(x)
+#         return x
+
+
+
+
+
+
+
+
+# class Stroke_Counter_L(nn.Module):
+#     def __init__(self):
+#         super(Stroke_Counter_L, self).__init__()
+#         self.conv1 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+#         self.bn1 = nn.BatchNorm2d(64)
+#         self.relu1 = nn.ReLU(inplace=True)
+#         self.conv2 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1)
+#         self.bn2 = nn.BatchNorm2d(32)
+#         self.relu2 = nn.ReLU(inplace=True)
+#         self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+#         self.linear = nn.Linear(32, 4)
+
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.bn1(x)
+#         x = self.relu1(x)
+#         x = self.conv2(x)
+#         x = self.bn2(x)
+#         x = self.relu2(x)
+
+#         x = self.avgpool(x)
+#         x.squeeze_(3).squeeze_(2)
+#         x = self.linear(x)
+#         return x
+
+
+# class Stroke_Counter(nn.Module):
+#     def __init__(self):
+#         super(Stroke_Counter, self).__init__()
+#         self.shared_CNN = Stroke_counter_CNNS()
+#         self.count_n = Stroke_Counter_N()
+#         self.count_l = Stroke_Counter_L()
+
+#     def forward(self, x):
+#         x = self.shared_CNN(x)
+#         pred_stroke_n = self.count_n(x)
+#         pred_stroke_l = self.count_l(x)
+#         return pred_stroke_n, pred_stroke_l
