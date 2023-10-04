6fb52ed82688d9a09343418a44c9cc025ff29ce1
Le Duc Minh <minhmeo7a1@gmail.com>
2023-10-04 14:37:11 +0700

    Change kaggle

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Config paths
 LABEL_FILE = 'data/train_gt.txt'
 SYNTH_LABEL_FILE = 'data/annotation2.txt'
-TRAIN_DIR = 'data/new_train/'
-PUBLIC_TEST_DIR = 'data/new_public_test/'
-SYNTH_TRAIN_DIR = 'data/synthesis/'  # SynthText
+# TRAIN_DIR = 'data/new_train/'
+# PUBLIC_TEST_DIR = 'data/new_public_test/'
+# SYNTH_TRAIN_DIR = 'data/synthesis/'  # SynthText
 
 # For Kaggle
-# PUBLIC_TEST_DIR = '/kaggle/input/ocr-v3/public_test_data/new_public_test'
-# TRAIN_DIR = '/kaggle/input/ocr-v3/training_data/new_train'
-# SYNTH_TRAIN_DIR = '/kaggle/input/ocr-v3/synthesis/synthesis'
+PUBLIC_TEST_DIR = '/kaggle/input/ocr-v3/public_test_data/new_public_test'
+TRAIN_DIR = '/kaggle/input/ocr-v3/training_data/new_train'
+SYNTH_TRAIN_DIR = '/kaggle/input/ocr-v3/synthesis/synthesis'
