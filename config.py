8c752174298a2639ac3f0a7699a533ae389f262e
Le Duc Minh <minhmeo7a1@gmail.com>
2023-10-07 23:48:26 +0700

    Change account

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 # Config paths
 LABEL_FILE = 'data/train_gt.txt'
-SYNTH_LABEL_FILE = 'data/annotation2.txt'
+# SYNTH_LABEL_FILE = 'data/annotation2.txt'
+SYNTH_LABEL_FILE = 'data/annotation.txt'
 # TRAIN_DIR = 'data/new_train/'
 # PUBLIC_TEST_DIR = 'data/new_public_test/'
 # SYNTH_TRAIN_DIR = 'data/synthesis/'  # SynthText
 
 # For Kaggle
-PUBLIC_TEST_DIR = '/kaggle/input/ocr-v3/public_test_data/new_public_test'
-TRAIN_DIR = '/kaggle/input/ocr-v3/training_data/new_train'
-SYNTH_TRAIN_DIR = '/kaggle/input/ocr-v3/synthesis/synthesis'
+PUBLIC_TEST_DIR = '/kaggle/input/ocr-hackathon-v4/public_test_data'
+TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/training_data'
+SYNTH_TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/synthesis_2'
