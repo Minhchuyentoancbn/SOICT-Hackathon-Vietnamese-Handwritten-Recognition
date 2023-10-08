ae7e764b6a9c2b0882e5a670836867f007f7d2d9
Le Duc Minh <minhmeo7a1@gmail.com>
2023-10-08 11:02:03 +0700

    Fix path error

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ LABEL_FILE = 'data/train_gt.txt'
 SYNTH_LABEL_FILE = 'data/annotation.txt'
 # TRAIN_DIR = 'data/new_train/'
 # PUBLIC_TEST_DIR = 'data/new_public_test/'
-# SYNTH_TRAIN_DIR = 'data/synthesis/'  # SynthText
+# SYNTH_TRAIN_DIR = 'data/synthesis2/'  # SynthText
 
 # For Kaggle
-PUBLIC_TEST_DIR = '/kaggle/input/ocr-hackathon-v4/public_test_data'
-TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/training_data'
-SYNTH_TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/synthesis_2'
+PUBLIC_TEST_DIR = '/kaggle/input/ocr-hackathon-v4/public_test_data/new_public_test'
+TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/training_data/new_train'
+SYNTH_TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/synthesis_2/synthesis_2'
