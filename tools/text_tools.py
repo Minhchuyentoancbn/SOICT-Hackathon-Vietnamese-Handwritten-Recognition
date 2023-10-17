549e9c113ce588753d72fff587fc2984990fd099
Le Duc Minh <minhmeo7a1@gmail.com>
2023-10-17 15:25:37 +0700

    Fix bugs

--- tools/text_tools.py
+++ tools/text_tools.py
@@ -151,11 +151,11 @@ def vietnamese_case_insensitive_sort_key(word):
     word = unicodedata.normalize('NFC', word)
     return [vietnamese_order_dict[c] for c in word.lower()]
 
-# pattern = "áàảãạắằẳẵặấầẩẫậèéẻẽẹếềểễệíìỉĩịóòỏõọốồổỗộớờởỡợúùủũụứừửữựýỳỷỹỵ"
-# replacement = "a"*5 + "ă"*5 + "â"*5 + "e"*5 + "ê"*5 + "i"*5 + "o"*5 + "ô"*5 + "ơ"*5 + "u"*5 + "ư"*5 + "y"*5
+pattern = "áàảãạắằẳẵặấầẩẫậèéẻẽẹếềểễệíìỉĩịóòỏõọốồổỗộớờởỡợúùủũụứừửữựýỳỷỹỵ"
+replacement = "a"*5 + "ă"*5 + "â"*5 + "e"*5 + "ê"*5 + "i"*5 + "o"*5 + "ô"*5 + "ơ"*5 + "u"*5 + "ư"*5 + "y"*5
 
-pattern = "ạặậ" + "ẹệ" + "ị" + "ọộợ" + "ụự" + "ỵ"
-replacement = "aăâ" + "eê" + "i" + "oôơ" + "uư" + "y"
+# pattern = "ạặậ" + "ẹệ" + "ị" + "ọộợ" + "ụự" + "ỵ"
+# replacement = "aăâ" + "eê" + "i" + "oôơ" + "uư" + "y"
 
 BANG_XOA_DAU = str.maketrans(
     pattern,
@@ -165,7 +165,8 @@ BANG_XOA_DAU = str.maketrans(
 def delete_diacritic(txt: str) -> str:
     if not unicodedata.is_normalized("NFC", txt):
         txt = unicodedata.normalize("NFC", txt)
-    return txt.translate(BANG_XOA_DAU)
+    txt = txt.translate(BANG_XOA_DAU)
+    return txt
 
 
 accent_dictionary = "aàáạảãâầấậẩẫăằắặẳẵAÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪeèéẹẻẽêềếệểễEÈÉẸẺẼÊỀẾỆỂỄoòóọỏõôồốộổỗơờớợởỡOÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠiìíịỉĩIÌÍỊỈĨuùúụủũưừứựửữƯỪỨỰỬỮUÙÚỤỦŨyỳýỵỷỹYỲÝỴỶỸ"
