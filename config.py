b96cbe744d02a5a090aedeb3323466077e6935a0
Le Duc Minh <minhmeo7a1@gmail.com>
2023-10-19 11:19:22 +0700

    Update path

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 # Config paths
 LABEL_FILE = 'data/train_gt.txt'
-# SYNTH_LABEL_FILE = 'data/annotation2.txt'
 SYNTH_LABEL_FILE = 'data/annotation.txt'
+SYNTH_LABEL_FILE2 = 'data/annotation2.txt'
 # TRAIN_DIR = 'data/new_train/'
 # PUBLIC_TEST_DIR = 'data/new_public_test/'
 # SYNTH_TRAIN_DIR = 'data/synthesis2/'  # SynthText
+# SYNTH_TRAIN_DIR2 = 'data/synthesis/'  # SynthText
 
 # For Kaggle
 PUBLIC_TEST_DIR = '/kaggle/input/ocr-hackathon-v4/public_test_data/new_public_test'
 TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/training_data/new_train'
 SYNTH_TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/synthesis_2/synthesis_2'
+SYNTH_TRAIN_DIR2 = '/kaggle/input/ocr-hackathon-v4/synthesis/synthesis'
